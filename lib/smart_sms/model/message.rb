1c0a06ea0251e2715bb00736f3ca0b690d7460df
谢刚 <tianbymy@163.com>
2017-06-24 07:06:08 +0800

    恢复

--- lib/smart_sms/model/message.rb
+++ lib/smart_sms/model/message.rb
@@ -1,7 +1,8 @@
 module SmartSMS
   # Message model to store sms messages
   #
-  class Message < ::ApplicationRecord
+  class Message < ::ActiveRecord::Base
+
     self.table_name = 'smart_sms_messages'
     belongs_to :smsable, polymorphic: true
     attr_accessible :sid, :uid, :mobile, :send_time, :text, :code, :send_status, :report_status, :fee, :user_receive_time, :error_msg if SmartSMS.active_record_protected_attributes?
